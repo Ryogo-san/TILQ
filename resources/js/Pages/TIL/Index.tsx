d674f0632c4fef752e26c71d793d41cb47a3b2a5
Ryogo-san <ritosetsugekka0226@gmail.com>
2022-10-16 19:45:52 +0900

    react-tsx

--- resources/js/Pages/TIL/Index.tsx
+++ resources/js/Pages/TIL/Index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import "@fullcalendar/react/dist/vdom"; // vite
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import Topbar from "@/MyComponents/Topbar";
 import { Link } from "@inertiajs/inertia-react";
+import * as THREE from "three";
 
 export default function Index(props) {
     const user = props.user;
@@ -30,12 +31,66 @@ export default function Index(props) {
         margin-left: auto;
     `;
 
+    const mountRef = useRef(null);
+
+    useEffect(() => {
+        const w = 640;
+        const h = 360;
+
+        // レンダラーを起動
+        const renderer = new THREE.WebGLRenderer();
+
+        const elm = mountRef.current;
+
+        elm?.appendChild(renderer.domElement);
+
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(w, h);
+
+        const scene = new THREE.Scene();
+
+        const camera = new THREE.PerspectiveCamera(45, w / h, 1, 10000);
+        camera.position.set(0, 0, +1000);
+        const geometry = new THREE.SphereGeometry(300, 30, 30);
+
+        const loader = new THREE.TextureLoader();
+
+        const texture = loader.load("../storage/images/block.jpg");
+
+        const material = new THREE.MeshStandardMaterial({
+            map: texture,
+        });
+
+        const mesh = new THREE.Mesh(geometry, material);
+
+        scene.add(mesh);
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(1, 1, 1);
+
+        scene.add(directionalLight);
+
+        const tick = () => {
+            mesh.rotation.y += 0.01;
+            renderer.render(scene, camera);
+
+            requestAnimationFrame(tick);
+        };
+
+        tick();
+
+        return () => {
+            elm?.removeChild(renderer.domElement);
+        };
+    }, []);
+
     return (
         <div>
             <Topbar user={props.auth.user} />
             <h1>Hello {user.name}-san!</h1>
             <h3> Here is the TIL Dashboard.</h3>
             <Link href="/til/mydashboard">Go to your dashboard</Link>
+            <div ref={mountRef} class="m-auto" />
             <Wrapper>
                 <FullCalendar
                     plugins={[dayGridPlugin]}
